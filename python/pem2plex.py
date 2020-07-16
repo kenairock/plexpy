64ce0d1cc31e4dc2529a7c5f9baab86a48960a97
kenairock <kenairock16@gmail.com>
2020-07-16 15:56:52 +0800

    Update pem2plex.py

--- python/pem2plex.py
+++ python/pem2plex.py
@@ -6,18 +6,15 @@ import hashlib
 from OpenSSL.crypto import *
 
 def main():
-  if(len(sys.argv) != 4):
-    print sys.argv[0] + " /path/to/ssl.crt /path/to/ssl.key ProcessedMachineIdentifier"
-    sys.exit(0)
   hash = hashlib.sha512()
-  hash.update('plex')
-  hash.update(sys.argv[3])
+  hash.update(('plex').encode('utf-8'))
+  hash.update((' [ProcessedMachineIdentifier] ').encode('utf-8')) #edit paste your identifier here without spaces
   passphrase = hash.hexdigest()
 
-  with open(sys.argv[1], 'rb') as f:
+  with open('e:\c.crt', 'rb') as f:
     c = f.read()
 
-  with open(sys.argv[2], 'rb') as f:
+  with open('e:\k.key', 'rb') as f:
     k = f.read()
 
   key = load_privatekey(FILETYPE_PEM,k)
@@ -25,9 +22,11 @@ def main():
   p12 = PKCS12()
   p12.set_certificate(cert)
   p12.set_privatekey(key)
-  open("certificate.p12", 'w' ).write( p12.export(passphrase) )
-
-  print passphrase
+  out_file = open("e:\certificate.p12", 'wb' )
+  out_file.write( p12.export(passphrase) )
+  
+  print(passphrase)
 
 if __name__ == '__main__':
   main()
+
